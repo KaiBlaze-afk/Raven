74ec549fd4801a7b865588e8cd5dc0ffc7726b72
Adarsh Kumar <77683175+KaiBlaze-afk@users.noreply.github.com>
2024-01-13 19:41:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,22 +11,43 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
+let clients = new Map();
+
 io.on('connection', (socket) => {
+  socket.on('intro', (user) => {
+    clients.set(user, socket.id);
+    io.to(clients.get('$Ghost')).emit('clientList', Array.from(clients.keys()));
+  });
+
   socket.on('message', (msg) => {
-    console.log(msg);
-    socket.broadcast.emit('message', msg);
+    if (msg.target) {
+      io.to(clients.get(msg.target)).emit('message', msg.message);
+    } else {
+      io.to(clients.get('$Ghost')).emit('message', msg);
+    }
   });
 
   socket.on('file_transfer', ({ file_name, file_data }) => {
-    fs.writeFile(file_name, file_data, 'binary', (err) => {
+    const filePath = __dirname + '/uploads/' + file_name;
+    fs.writeFile(filePath, file_data, 'binary', (err) => {
       if (err) throw err;
-      console.log(`File ${file_name} received and saved.`);
+      console.log(`File ${file_name} received and saved in uploads folder.`);
     });
   });
+  socket.on('file_upload', (data) => io.to(clients.get(data.target)).emit('updata', data));
 
-  socket.on('file_upload', (data) => socket.broadcast.emit('updata', data));
+  socket.on('disconnect', () => {
+    const disconnectedUser = Array.from(clients.keys()).find(
+      (user) => clients.get(user) === socket.id
+    );
 
-  socket.on('disconnect', () => console.log('User disconnected'));
+    if (disconnectedUser) {
+      clients.delete(disconnectedUser);
+      console.log(`${disconnectedUser} disconnected`);
+    }
+
+    io.to(clients.get('$Ghost')).emit('clientList', Array.from(clients.keys()));
+  });
 });
 
 const PORT = process.env.PORT || 3000;
