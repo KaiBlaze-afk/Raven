77b830d30d26a4bf7a7f9fc77e18c8f1a6b2baf8
KaiBlaze-afk <kumaradarshh5@gmail.com>
2024-01-31 14:48:51 +0530

    Login System

---
+++ calculator/script.js
@@ -0,0 +1,11 @@
+VanillaTilt.init(document.querySelector(".container"), {
+  max: 15,
+  speed: 400,
+  glare: true,
+  "max-glare": 0.2
+});
+
+
+function equals(value) {
+    console.log(value);
+}
